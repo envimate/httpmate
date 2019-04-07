5f9e342a9729f3bbf8ac76e4ada7d7af7f766c04
envimate opensource <opensource@envimate.com>
2019-04-07 16:20:15 +0200

    save

---
+++ core/src/main/java/com/envimate/httpmate/chains/ChainName.java
@@ -0,0 +1,20 @@
+package com.envimate.httpmate.chains;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import static com.envimate.httpmate.util.Validators.validateNotNullNorEmpty;
+
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ChainName {
+    private final String name;
+
+    public static ChainName chainName(final String name) {
+        validateNotNullNorEmpty(name, "name");
+        return new ChainName(name);
+    }
+}
---
+++ core/src/main/java/com/envimate/httpmate/chains/builder/ChainBuilder.java
@@ -0,0 +1,10 @@
+package com.envimate.httpmate.chains.builder;
+
+import com.envimate.httpmate.chains.ChainName;
+
+public final class ChainBuilder {
+
+    public static ChainBuilder startingAChainWith(final ChainName chainName) {
+        throw new UnsupportedOperationException();
+    }
+}
